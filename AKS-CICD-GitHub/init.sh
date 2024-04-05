ca776bdd6361f0bb7afd992ab9fad4a1fba7c2b1
Zulfia <146954022+ZCHAnalytics@users.noreply.github.com>
2024-04-05 10:28:47 +0100

    Update init.sh

--- AKS-CICD-GitHub/init.sh
+++ AKS-CICD-GitHub/init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Defining variables..."
-export RESOURCE_GROUP_NAME=mslearn-gh-pipelines-$RANDOM
+export RESOURCE_GROUP_NAME=github-pipelines-$RANDOM
 export AKS_NAME=contoso-video
-export ACR_NAME=contosocontainerregistry$RANDOM
+export ACR_NAME=contosoacr$RANDOM
 
 echo "Searching for resource group..."
-az group create -n $RESOURCE_GROUP_NAME -l uksouth
+az group create -n $RESOURCE_GROUP_NAME -l uksouth # add region
 
 echo "Creating cluster..."
 az aks create \
   --resource-group $RESOURCE_GROUP_NAME \
   --name $AKS_NAME \
-  --location uksouth \
+  --location uksouth \ # add the same region as above
   --node-count 1 \
   --enable-addons http_application_routing \
   --dns-name-prefix $AKS_NAME \
@@ -24,9 +24,11 @@ echo "Obtaining credentials..."
 az aks get-credentials -n $AKS_NAME -g $RESOURCE_GROUP_NAME
 
 echo "Creating ACR..."
-az acr create -n $ACR_NAME -g $RESOURCE_GROUP_NAME --sku basic --location uksouth
+az acr create -n $ACR_NAME -g $RESOURCE_GROUP_NAME --sku basic --location uksouth # same region as above
 az acr update -n $ACR_NAME --admin-enabled true
 
+export ACR_USERNAME=$(az acr credential show -n $ACR_NAME --query "username" -o tsv)
+export ACR_PASSWORD=$(az acr credential show -n $ACR_NAME --query "passwords[0].value" -o tsv)
 export ACR_USERNAME=$(az acr credential show -n $ACR_NAME --query "username" -o tsv)
 export ACR_PASSWORD=$(az acr credential show -n $ACR_NAME --query "passwords[0].value" -o tsv)
 
@@ -34,16 +36,17 @@ az aks update \
     --name $AKS_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
     --attach-acr $ACR_NAME
+    --attach-acr $ACR_NAME
 
-export DNS_NAME=$(az network dns zone list --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
+export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
 
-sed -i 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
-sed -i 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
+sed -i '' 's+!IMAGE!+'"$ACR_NAME"'.azurecr.io/contoso-website+g' kubernetes/deployment.yaml # Add `.azurecr.io` after ACR_NAME variable
+sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
 
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
 echo "-> Resource Group Name: $RESOURCE_GROUP_NAME"
-echo "-> ACR Name: $ACR_NAME"
+echo "-> ACR Name: $ACR_NAME.azurecr.io" Add `.azurecr.io` after ACR_NAME variable
 echo "-> ACR Login Username: $ACR_USERNAME"
 echo "-> ACR Password: $ACR_PASSWORD"
-echo "-> AKS Cluster Name: $ACR_NAME"
+echo "-> AKS Cluster Name: $AKS_NAME"
 echo "-> AKS DNS Zone Name: $DNS_NAME"
