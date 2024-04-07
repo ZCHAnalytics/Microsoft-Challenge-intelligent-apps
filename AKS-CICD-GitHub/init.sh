cdf1afe773973f40af5daebe6eda49fa46c01c7e
Zulfia <146954022+ZCHAnalytics@users.noreply.github.com>
2024-04-07 10:25:53 +0100

    Update init.sh

--- AKS-CICD-GitHub/init.sh
+++ AKS-CICD-GitHub/init.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 echo "Defining variables..."
-export RESOURCE_GROUP_NAME=github-pipelines-$RANDOM
+export RESOURCE_GROUP_NAME=mslearn-gh-pipelines-$RANDOM
 export AKS_NAME=contoso-video
-export ACR_NAME=contosoacr$RANDOM
+export ACR_NAME=contosocontainerregistry$RANDOM
 
 echo "Searching for resource group..."
-az group create -n $RESOURCE_GROUP_NAME -l uksouth # add region
+az group create -n $RESOURCE_GROUP_NAME -l uksouth # Note the region
 
+# Add location flag to ensure AKS is created in the same region as the resource group 
 echo "Creating cluster..."
 az aks create \
   --resource-group $RESOURCE_GROUP_NAME \
   --name $AKS_NAME \
-  --location uksouth \ # add the same region as above
   --node-count 1 \
   --enable-addons http_application_routing \
   --dns-name-prefix $AKS_NAME \
   --enable-managed-identity \
   --generate-ssh-keys \
-  --node-vm-size Standard_B2s
+  --node-vm-size Standard_B2s \
+  --location uksouth
 
 echo "Obtaining credentials..."
 az aks get-credentials -n $AKS_NAME -g $RESOURCE_GROUP_NAME
 
 echo "Creating ACR..."
-az acr create -n $ACR_NAME -g $RESOURCE_GROUP_NAME --sku basic --location uksouth # same region as above
+az acr create -n $ACR_NAME -g $RESOURCE_GROUP_NAME --sku basic
 az acr update -n $ACR_NAME --admin-enabled true
 
 export ACR_USERNAME=$(az acr credential show -n $ACR_NAME --query "username" -o tsv)
@@ -37,13 +38,34 @@ az aks update \
 
 export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
 
-sed -i '' 's+!IMAGE!+'"$ACR_NAME"'.azurecr.io/contoso-website+g' kubernetes/deployment.yaml # Add `.azurecr.io` after ACR_NAME variable
-sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
+# Thse commands won't work because: 
+# a) deployment.yaml and ingress.yaml are located in different folders which have not been created; 
+# b) these two files use placeholders referencing values.yaml file to create ACR_NAME and DNS_NAME values. 
+
+# Solution 1 
+# Change values in values.yaml file manually after the initialisation complete. and remove SED commands from init.sh as they are redundant. 
+# This way, the deployment and ingress files will be automatically updated with values from values.yaml file
+# Action for solution 1
+# remove sed commands in init.sh 
+
+# Option 2 Create values.yaml file and replace values with outputs from init.sh file
+mkdir -p kubernetes/contoso-website
+cat <<EOF > kubernetes/contoso-website/values.yaml
+image:
+  registry: ${ACR_NAME}.azurecr.io
+  name: contoso-website
+  tag: latest
+
+dns:
+  name: ${DNS_NAME}
+EOF
 
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
 echo "-> Resource Group Name: $RESOURCE_GROUP_NAME"
-echo "-> ACR Name: $ACR_NAME.azurecr.io" Add `.azurecr.io` after ACR_NAME variable
+# echo "-> ACR Name: $ACR_NAME"
+echo "-> ACR Name: $ACR_NAME.azurecr.io" # append `.azurecr.io` to ACR_NAME
 echo "-> ACR Login Username: $ACR_USERNAME"
 echo "-> ACR Password: $ACR_PASSWORD"
+# Chnage incorrect name ACR_NAME to AKS_NAME
 echo "-> AKS Cluster Name: $AKS_NAME"
 echo "-> AKS DNS Zone Name: $DNS_NAME"
